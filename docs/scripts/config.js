d520a891efccd5f21e83f3e46b6340e9de19cd42
mbrzozo <mbrzozo@gmail.com>
2022-06-06 14:52:34 +0200

    go

--- docs/scripts/config.js
+++ docs/scripts/config.js
@@ -2,7 +2,7 @@ export default {
     // Token Implicit Grant Client ID
     clientID: '3616bef8-4b6c-4eda-ab17-b94fb26d72f2',
 
-    redirectUri: 'https://apps.mypurecloud.de/admin/#/admin/oauth/authorizations/3616bef8-4b6c-4eda-ab17-b94fb26d72f2',
+    redirectUri: 'https://3.73.26.20',
 
     genesysCloud: {
         // Genesys Cloud region
